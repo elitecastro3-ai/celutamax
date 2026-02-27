88ca0baec2673e0f14f904df87269b2e21dcdd7c
elitecastro3-ai <elitecastro3@gmail.com>
2026-02-27 19:39:05 +0300

    Fix template issues and other site updates

--- celutamax/urls.py
+++ celutamax/urls.py
@@ -2,13 +2,13 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-
-from vehicles.views import home, vehicle_list, about, contact
+from vehicles.views import home, vehicle_list, vehicle_detail, about, contact
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'),   # THIS is the homepage
     path('vehicles/', vehicle_list, name='vehicles'),
+    path('vehicles/<int:pk>/', vehicle_detail, name='vehicle_detail'),
     path('about/', about, name='about'),
     path('contact/', contact, name='contact'),
 ]
--- vehicles/views.py
+++ vehicles/views.py
@@ -1,18 +1,42 @@
 from django.shortcuts import render
+from django.db.models import Q
+from .models import Vehicle
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
-from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
-from django.shortcuts import render    
-from .models import Vehicle
+
 
 def vehicle_list(request):
-    vehicles = Vehicle.objects.all()
-    return render(request, 'vehicles.html', {'vehicles': vehicles})
+    query = request.GET.get('q')
+
+    if query:
+        vehicles = Vehicle.objects.filter(
+            Q(title__icontains=query) |
+            Q(brand__icontains=query) |
+            Q(description__icontains=query) |
+            Q(year__icontains=query) |
+            Q(mileage__icontains=query)
+        )
+    else:
+        vehicles = Vehicle.objects.all()
+
+    return render(request, 'vehicles/vehicle_list.html', {'vehicles': vehicles})
+
+
 def about(request):
     return render(request, 'about.html')
 
+
 def contact(request):
     return render(request, 'contact.html')
+
+
+def vehicle_detail(request, pk):
+    vehicle = get_object_or_404(Vehicle, pk=pk)
+    return render(request, 'vehicles/vehicle_detail.html', {'vehicle': vehicle})
+    
+
+
+    
