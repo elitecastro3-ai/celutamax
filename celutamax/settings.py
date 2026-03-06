5555a3cb24a92a040b4994219b8cbfce0909f76c
elitecastro3-ai <elitecastro3@gmail.com>
2026-03-06 18:17:59 +0300

    fixed database config

--- celutamax/settings.py
+++ celutamax/settings.py
@@ -90,13 +90,12 @@ STATICFILES_DIR = [BASE_DIR / 'static']
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': { dj_database_url.config(
+    'default': dj_database_url.config(
         default=os.environ.get('DATABASE_URL')
+        conn_max_age=600
     )
         
     }
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
