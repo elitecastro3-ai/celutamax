e9145b3d0fcd392dbca523dfcf3558fb8f635334
elitecastro3-ai <elitecastro3@gmail.com>
2026-03-08 14:54:57 +0300

    simplify sitemap for debugging

--- vehicles/sitemaps.py
+++ vehicles/sitemaps.py
@@ -3,11 +3,9 @@ from .models import Vehicle
 
 
 class VehicleSitemap(Sitemap):
-    changefreq = "daily"
-    priority = 0.9
+    
 
     def items(self):
         return Vehicle.objects.all()
 
-    def lastmod(self, obj):
-        return obj.created_at
+   
