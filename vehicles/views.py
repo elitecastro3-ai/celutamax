5a3c58e82ba5a079abe3fd3ea61ad70665aed9c7
elitecastro3-ai <elitecastro3@gmail.com>
2026-03-08 19:06:11 +0300

    fix syntax error in vehicle_list view

--- vehicles/views.py
+++ vehicles/views.py
@@ -1,7 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.db.models import Q
 from .models import Vehicle
-from django.shortcuts import get_object_or_404
 
 
 def home(request):
@@ -11,15 +10,14 @@ def home(request):
 def vehicle_list(request):
     query = request.GET.get('q')
 
+    vehicles = Vehicle.objects.all()
+
     if query:
-        vehicles = Vehicle.objects.filter(
+        vehicles = vehicles.filter(
             Q(title__icontains=query) |
             Q(brand__icontains=query) |
-            Q(description__icontains=query) |
-            
+            Q(description__icontains=query)
         )
-    else:
-        vehicles = Vehicle.objects.all()
 
     return render(request, 'vehicles/vehicle_list.html', {'vehicles': vehicles})
 
@@ -35,7 +33,3 @@ def contact(request):
 def vehicle_detail(request, pk):
     vehicle = get_object_or_404(Vehicle, pk=pk)
     return render(request, 'vehicles/vehicle_detail.html', {'vehicle': vehicle})
-    
-
-
-    
