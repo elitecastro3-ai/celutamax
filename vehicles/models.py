b0a41d075653c8f355ad0d95491e1cf8f1705a86
elitecastro3-ai <elitecastro3@gmail.com>
2026-03-08 14:03:54 +0300

    fix sitemap setup

--- vehicles/models.py
+++ vehicles/models.py
@@ -15,9 +15,8 @@ class Vehicle(models.Model):
 
     def __str__(self):
         return self.title
-     
-       
     
+     
     def get_absolute_url(self):
         return reverse("vehicle_detail", agrs=[self.pk])    
 
