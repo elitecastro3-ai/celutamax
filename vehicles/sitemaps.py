54f2dd0a2b9e4181167bc83fc7ebd2585109e97c
elitecastro3-ai <elitecastro3@gmail.com>
2026-03-11 15:29:08 +0300

    show right error

--- vehicles/sitemaps.py
+++ vehicles/sitemaps.py
@@ -3,6 +3,8 @@ from .models import Vehicle
 
 
 class VehicleSitemap(Sitemap):
+    changefreq = "weekly"
+    priority = 0.8
     
 
     def items(self):
@@ -10,5 +12,11 @@ class VehicleSitemap(Sitemap):
     
     def location(self, obj):
         return obj.get_absolute_url()
+
+    def lastmod(self, obj):
+        return obj.created_at
+
+    def location(self, obj):
+        return obj.get_absolute_url()    
 
    
